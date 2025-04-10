5eb3612b7b94c82efb1d86db067c1a2aa29416b9
Dakota Stammen <134533986+TerminalProdigy@users.noreply.github.com>
2025-04-10 15:15:22 -0400

    PR2 (#1)
    * Branch: feature/foo | Commit #1
    
    * Branch: feature/foo | Commit #3
    
    * Branch: feature/foo | Commit #4

---
+++ Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMS.Demo.Git.Branching.GitHubFlow.Squash_MergePR.Good01
+{
+    class Class2
+    {
+        // Branch: feature/foo | Commit #1
+        // Branch: feature/foo | Commit #3
+        // Branch: feature/foo | Commit #4
+    }
+}
